5a0624576971f3fd526bcabeefa270313d75c314
Hassin-bono <bloodygamer3212@gmail.com>
2026-07-27 23:36:37 +0500

    Delete users.py

--- users.py
+++
@@ -1,8 +0,0 @@
-# users.py
-
-def get_user_details(user_id: int):
-    """
-    Fetch user details from database.
-    Updated signature: 'include_metadata' parameter removed.
-    """
-    return {"id": user_id, "name": "Jane Doe"
